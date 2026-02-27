362c1e0ec01cd2ba31303263da0c056a72371564
Student <tidalwavegames4@gmail.com>
2026-02-27 21:47:30 +0000

    Fershing

--- Assets/Scripts/GameVariables.cs
+++ Assets/Scripts/GameVariables.cs
@@ -5,7 +5,10 @@ public static class GameVariables
 {
     public static TimeSpan gameTimer = new TimeSpan(0, 3, 0);
 
-    public static bool playerCanMove = true, playerCanLook = true;
+    public static bool playerCanMove = true, playerCanLook = true, playerFishing = false;
+
+    public static bool playerHasHunger = false, playerHasEnergy = false;
+    public static int playerHunger = 100, playerEnergy = 100;
     
     public static float mouseSensitivityMulti = 1;
 }
--- Assets/Scripts/Minigames/FishingSpot.cs
+++ Assets/Scripts/Minigames/FishingSpot.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 public class FishingSpot : AbstractInteractableObject
 {
 
-    public GameObject fishRod, fishingUI;
-    public Animator fishRodAnimator;
-
+    public GameObject fishingUI;
+    
     public Transform playerMoveToFish;
 
     public List<Fish> possibleFish = new();
 
-    bool goneFishin = false;
     string originalName;
     public override void OnInteract()
     {
         base.OnInteract();
 
-        goneFishin = true;
+        if (GameVariables.playerFishing) return;
+
         FindAnyObjectByType<FishingManager>(FindObjectsInactive.Include).currentFishingSpot = this;
         fishingUI.SetActive(true);
         originalName = displayName;
